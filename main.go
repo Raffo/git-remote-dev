88a12f8cdba8b8b5e63a5550498e4c79f3f0ec60
Raffaele Di Fazio <difazio.raffaele@gmail.com>
2019-12-27 12:05:08 +0100

    adds pull and make
    Signed-off-by: Raffaele Di Fazio <difazio.raffaele@gmail.com>

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/Sirupsen/logrus"
@@ -11,8 +13,20 @@ import (
 	"gopkg.in/src-d/go-git.v4/plumbing"
 )
 
+func execCmd(command string) []byte {
+	logrus.Printf("running %s\n", command)
+	commandAndArgs := strings.Split(command, " ")
+	cmd := exec.Command(commandAndArgs[0], commandAndArgs[1:]...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		logrus.Infoln(string(out))
+		logrus.Errorln(err)
+	}
+	return out
+}
+
 // pullIfNeeded checks if there was an update to a specified branch
-func pullIfNeeded(branch string) {
+func pullIfNeeded(branch string) bool {
 	dir, err := os.Getwd()
 	if err != nil {
 		logrus.Errorf("error getting current directory: %v", err)
@@ -31,15 +45,19 @@ func pullIfNeeded(branch string) {
 	if err != nil {
 		logrus.Errorf("error checking out repo: %v", err)
 	}
+
 	err = w.Pull(&git.PullOptions{RemoteName: "origin"})
 	if err != nil {
 		logrus.Errorf("error pulling repo: %v", err)
 	}
+
+	return true // now always reloading
 }
 
 func main() {
 	branch := kingpin.Flag("branch", "branch to sync").Required().String()
 	interval := kingpin.Flag("interval", "interval for the sync loop").Default("1m").Duration()
+	command := kingpin.Flag("command", "command to run after the sync").Default("make run").String()
 	kingpin.Parse()
 
 	stopChannel := make(chan int)
@@ -47,7 +65,11 @@ func main() {
 	for {
 		select {
 		case <-time.After(*interval):
-			pullIfNeeded(*branch)
+			changed := pullIfNeeded(*branch)
+			if changed {
+				execCmd(*command)
+			}
+
 		case <-stopChannel:
 			logrus.Infoln("stop requested, exiting.")
 			return
